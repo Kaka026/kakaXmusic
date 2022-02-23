25db908ea68895321aae3b6153412703aae9bd65
Kaka026 <96742314+Kaka026@users.noreply.github.com>
2022-02-23 13:16:12 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -18,7 +18,7 @@ ACTV_CALLS = []
 @authorized_users_only
 async def pause(_, message: Message):
     await callsmusic.pytgcalls.pause_stream(message.chat.id)
-    await message.reply_text("▶️ ᴘᴀᴜsᴇ ᴛʜᴇ sᴏɴɢ ")
+    await message.reply_text("▶️ ᴘᴀᴜsᴇ ᴛʜᴇ sᴏɴɢ ᴊᴀɴᴇᴍᴀɴ🖤 ")
 
 
 @Client.on_message(command(["resume"]) & other_filters)
@@ -26,7 +26,7 @@ async def pause(_, message: Message):
 @authorized_users_only
 async def resume(_, message: Message):
     await callsmusic.pytgcalls.resume_stream(message.chat.id)
-    await message.reply_text("⏸ ʀᴇsᴜᴍᴇ ᴛʜᴇ sᴏɴɢ")
+    await message.reply_text("⏸ ʀᴇsᴜᴍᴇ ᴛʜᴇ sᴏɴɢ ᴊᴀɴᴇᴍᴀɴ🖤")
 
 
 @Client.on_message(command(["end"]) & other_filters)
@@ -39,7 +39,7 @@ async def stop(_, message: Message):
         pass
 
     await callsmusic.pytgcalls.leave_group_call(message.chat.id)
-    await message.reply_text("❌ sᴛᴏᴘ ᴛʜᴇ ᴍᴜsɪᴄ")
+    await message.reply_text("❌ sᴛᴏᴘ ᴛʜᴇ ᴍᴜsɪᴄ ᴊᴀɴᴇᴍᴀɴ🖤 ")
 
 @Client.on_message(command(["skip"]) & other_filters)
 @errors
@@ -50,7 +50,7 @@ async def skip(_, message: Message):
     for x in callsmusic.pytgcalls.active_calls:
         ACTV_CALLS.append(int(x.chat_id))
     if int(chat_id) not in ACTV_CALLS:
-        await message.reply_text("ɴᴏᴛʜɪɴɢ ɪs ᴘʟᴀʏɪɴɢ")
+        await message.reply_text("ɴᴏᴛʜɪɴɢ ɪs ᴘʟᴀʏɪɴɢ ʙᴀʙʏ🖤 ")
     else:
         queues.task_done(chat_id)
         
@@ -65,4 +65,4 @@ async def skip(_, message: Message):
                     ),
                 ),
             )
-    await message.reply_text("sᴋɪᴘ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ sᴏɴɢ")
+    await message.reply_text("ᴛʀᴀᴄᴋ sᴋɪᴘᴘᴇᴅ ᴊᴀɴᴇᴍᴀɴ🖤")
