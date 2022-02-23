381b5cb2c8b29fae7dcec78d93e65c2ec0f3ef5b
Kaka026 <96742314+Kaka026@users.noreply.github.com>
2022-02-23 14:14:38 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -256,7 +256,7 @@ async def play(_, message: Message):
             return await lel.edit(
                 "Gɪᴠᴇ ᴍᴜsɪᴄ ɴᴀᴍᴇ ᴛᴏ ᴘʟᴀʏ**"
             )
-        await lel.edit("🎲")
+        await lel.edit("💥")
         query = message.text.split(None, 1)[1]
         # print(query)
         try:
